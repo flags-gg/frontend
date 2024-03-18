209f04d818afa890ac9eed5271680ded23bac528
Keloran <keloran@develbox.info>
2024-03-18 22:19:39 +0000

    refactor: Refactor App component rendering and add dark theme
    Updated the render method in the index file to include the ThemeProvider with a dark theme and the CssBaseline component. Also, wrapped the App component with the AuthProvider component. Removed the test case for the learn react link.
    
    feat: Update dependencies to latest versions
    
    Updated dependencies including @mui/material, @emotion/react, and @mui/icons-material to their latest versions. Also added oidc-client-ts, react-oidc-context, and react-router-dom dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {useAuth} from "react-oidc-context";
+
+import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Homepage";
 
 function App() {
+  const auth = useAuth()
+
+  useEffect(() => {
+    return auth.events.addAccessTokenExpired((error) => {
+      auth.signoutSilent().catch(console.error)
+    })
+  }, [auth])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {auth?.isAuthenticated ? (
+          <Route path={"/"} element={<Dashboard />} />
+        ) : (
+          <Route path={"/"} element={<Homepage />} />
+        )}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
