395ed481e808db354a174f6d5f590401cfd9c14f
Keloran <keloran@develbox.info>
2024-03-22 11:32:50 +0000

    feat: Update Keycloak secret handling in Kubernetes deployment
    Update Kubernetes deployment file to use the REACT_APP_KEYCLOAK_SECRET
    environment variable instead of individual secret keys for Keycloak
    configuration. This improves security and simplifies the deployment setup.

--- src/app.config.tsx
+++ src/app.config.tsx
@@ -4,7 +4,7 @@ import {AuthProviderProps} from "react-oidc-context";
 export const oidcConfig = {
   authority: "https://keys.chewedfeed.com/realms/flags-gg",
   client_id: "dashboard",
-  client_secret: "uFBHtS1wJ9p5Epbw2wWHuBYVmK2tQzhc",
+  client_secret: process.env.REACT_APP_KEYCLOAK_SECRET,
   redirect_uri: window.location.origin,
   onSigninCallback: (_user: User | void) => {
     window.history.replaceState({}, document.title, window.location.pathname)
