ce9b471a986d384e019d580e01509fd326290c10
Keloran <keloran@develbox.info>
2024-05-16 23:12:05 +0100

    refactor: Remove unnecessary theme mode assignment in useChartOptions function

--- src/pages/Dashboard/components/Requests/index.tsx
+++ src/pages/Dashboard/components/Requests/index.tsx
@@ -6,7 +6,6 @@ import {ApexOptions} from "apexcharts";
 // eslint-disable-next-line react-refresh/only-export-components
 export function useChartOptions(categories: string[], legend?: boolean): ApexOptions {
   const theme = useTheme()
-  theme.palette.mode = 'dark'
 
   const options = {
     chart: {
