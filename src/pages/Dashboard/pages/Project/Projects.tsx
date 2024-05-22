002cfc4366cae63376d747ed3ea715491bb82e66
Keloran <keloran@develbox.info>
2024-05-22 22:50:39 +0100

    feat: Fetch project limits and projects on Projects page
    Added functions to fetch limits and projects from the API.
    Updated Projects component to fetch and display projects based on fetched limits.
    Used useEffect to fetch data on component mount.

--- src/pages/Dashboard/pages/Project/Projects.tsx
+++ src/pages/Dashboard/pages/Project/Projects.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {FC, useEffect, useState} from "react";
 import {Project} from "@DC/ProjectSelector/types";
 import useAuthFetch from "@DL/fetcher";
 
@@ -6,6 +6,37 @@ export const Projects: FC = () => {
   const authFetch = useAuthFetch();
   const [projects, setProjects] = useState<Project[]>([]);
   const [allowedProjects, setAllowedProjects] = useState<number>(1);
+
+  const fetchLimits = async () => {
+    try {
+      const response = await authFetch("/company/limits");
+      return await response.json();
+    } catch (error) {
+      console.error("failed to fetch limits", error);
+    }
+  }
+  const fetchProjects = async () => {
+    try {
+      const response = await authFetch("/projects");
+      return await response.json();
+    } catch (error) {
+      console.error("failed to fetch projects", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchLimits().then((limits) => {
+      setAllowedProjects(limits?.projects);
+    }).catch((error) => {
+      console.error("failed to fetch limits", error);
+    })
+    fetchProjects().then((projects) => {
+      setProjects(projects);
+    }).catch((error) => {
+      console.error("failed to fetch projects", error);
+    })
+  }, []);
+
 
   return (
     <div>
